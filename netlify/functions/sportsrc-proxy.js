e1e483e06af20b17118223208772c3cb91c2cea9
nov4 <kitsuika.who2@gmail.com>
2026-03-04 16:20:38 +0700

    Create sportsrc-proxy.js

---
+++ netlify/functions/sportsrc-proxy.js
@@ -0,0 +1,35 @@
+// netlify/functions/sportsrc-proxy.js
+// Proxy untuk api.sportsrc.org agar tidak kena CORS block di browser
+
+exports.handler = async (event) => {
+  const params = event.queryStringParameters || {};
+  const query = new URLSearchParams(params).toString();
+  const url = `https://api.sportsrc.org/?${query}`;
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0',
+        'Accept': 'application/json',
+      }
+    });
+
+    const text = await response.text();
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+      body: text,
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+};
