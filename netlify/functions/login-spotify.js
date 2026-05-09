816ba42d141b07a739efce924fe309f2417aea50
nov4 <kitsuika.who2@gmail.com>
2026-05-09 11:10:26 +0700

    Update login-spotify.js

--- netlify/functions/login-spotify.js
+++ netlify/functions/login-spotify.js
@@ -1,7 +1,7 @@
 exports.handler = async () => {
     const clientId = process.env.SPOTIFY_CLIENT_ID;
     const redirectUri = 'https://novascatia.my.id/.netlify/functions/callback';
-    const scope = 'user-read-currently-playing user-modify-playback-state user-read-playback-state streaming user-read-email user-read-private';
+    const scope = 'user-read-currently-playing user-modify-playback-state user-read-playback-state streaming user-read-email user-read-private user-read-recently-played user-top-read';
 
     const authUrl = 'https://accounts.spotify.com/authorize?' +
         `response_type=code&client_id=${clientId}` +
