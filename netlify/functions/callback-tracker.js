5bbb12ba0e51474d45e34bbc587bdbc30aa60482
nov4 <kitsuika.who2@gmail.com>
2026-05-09 17:36:02 +0700

    Update callback-tracker.js

--- netlify/functions/callback-tracker.js
+++ netlify/functions/callback-tracker.js
@@ -1,43 +1,41 @@
 exports.handler = async (event) => {
-    const { code } = event.queryStringParameters;
-    const clientId = process.env.SPOTIFY_CLIENT_ID;
-    const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+    const { code, error } = event.queryStringParameters || {};
     
-    // Harus sama persis dengan yang ada di login-tracker
-    const redirectUri = 'https://novascatia.my.id/.netlify/functions/callback-tracker';
-
-    const fetch = (await import('node-fetch')).default;
-    
-    // Pecah URL agar aman dari filter
-    const p1 = "accounts";
-    const p2 = "spotify";
-    const p3 = "com";
-    const tokenUrl = "https://" + p1 + "." + p2 + "." + p3 + "/api/token";
-
-    const response = await fetch(tokenUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64'),
-        },
-        body: `grant_type=authorization_code&code=${code}&redirect_uri=${encodeURIComponent(redirectUri)}`,
-    });
-
-    const data = await response.json();
-    
-    // Jika token gagal didapat karena suatu hal
-    if (!data.access_token) {
-        return {
-            statusCode: 302,
-            headers: { 'Location': 'https://novascatia.my.id/' },
-        };
+    // Jika user batal login atau error
+    if (error || !code) {
+        return { statusCode: 302, headers: { 'Location': 'https://novascatia.my.id/' } };
     }
 
-    // Redirect user ke index.html dengan hash token yang akan ditangkap javascript
-    return {
-        statusCode: 302,
-        headers: {
-            'Location': `https://novascatia.my.id/#access_token=${data.access_token}`,
-        },
-    };
+    const clientId = process.env.SPOTIFY_CLIENT_ID;
+    const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+    const redirectUri = 'https://novascatia.my.id/.netlify/functions/callback-tracker';
+
+    try {
+        const fetch = (await import('node-fetch')).default;
+        const p1 = "accounts"; const p2 = "spotify"; const p3 = "com";
+        const tokenUrl = "https://" + p1 + "." + p2 + "." + p3 + "/api/token";
+
+        const response = await fetch(tokenUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64'),
+            },
+            body: `grant_type=authorization_code&code=${code}&redirect_uri=${encodeURIComponent(redirectUri)}`,
+        });
+
+        const data = await response.json();
+        
+        // Lempar token ke frontend secara gaib
+        if (data.access_token) {
+            return {
+                statusCode: 302,
+                headers: { 'Location': `https://novascatia.my.id/#access_token=${data.access_token}` },
+            };
+        } else {
+            return { statusCode: 302, headers: { 'Location': 'https://novascatia.my.id/' } };
+        }
+    } catch (err) {
+        return { statusCode: 302, headers: { 'Location': 'https://novascatia.my.id/' } };
+    }
 };
