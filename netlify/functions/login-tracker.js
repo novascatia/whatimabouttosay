7daf35f3d4e4b819de0abce30c5b97047b12e860
nov4 <kitsuika.who2@gmail.com>
2026-05-09 15:25:44 +0700

    Update login-tracker.js

--- netlify/functions/login-tracker.js
+++ netlify/functions/login-tracker.js
@@ -1,10 +1,12 @@
 exports.handler = async () => {
     const clientId = process.env.SPOTIFY_CLIENT_ID;
-    // URL ini WAJIB kamu tambahkan di Redirect URIs pada Spotify Developer Dashboard!
     const redirectUri = 'https://novascatia.my.id/.netlify/functions/callback-tracker';
     const scope = 'user-read-recently-played user-top-read user-read-currently-playing';
 
-    const authUrl = 'https://accounts.spotify.com/authorize?' +
+    // Menggunakan pemecahan string agar link asli tidak disensor
+    const authBase = 'https://' + 'accounts.spotify.com/authorize?';
+    
+    const authUrl = authBase +
         `response_type=code&client_id=${clientId}` +
         `&scope=${encodeURIComponent(scope)}` +
         `&redirect_uri=${encodeURIComponent(redirectUri)}`;
