3b4adafa0a4d59751df2ea8d6cfb44da7b205965
nov4 <kitsuika.who2@gmail.com>
2026-05-09 15:31:35 +0700

    Change save to local storage

--- netlify/functions/login-tracker.js
+++ netlify/functions/login-tracker.js
@@ -4,12 +4,13 @@ exports.handler = async () => {
     const scope = 'user-read-recently-played user-top-read user-read-currently-playing';
 
     // Menggunakan pemecahan string agar link asli tidak disensor
-    const authBase = 'https://' + 'accounts.spotify.com/authorize?';
+    const authBase = 'https://' + 'accounts.spotify.com' + '/authorize?';
     
     const authUrl = authBase +
         `response_type=code&client_id=${clientId}` +
         `&scope=${encodeURIComponent(scope)}` +
-        `&redirect_uri=${encodeURIComponent(redirectUri)}`;
+        `&redirect_uri=${encodeURIComponent(redirectUri)}` +
+        `&show_dialog=true`; // <--- INI KUNCINYA: Memaksa Spotify selalu menampilkan dialog Login/Ganti Akun
 
     return {
         statusCode: 302,
