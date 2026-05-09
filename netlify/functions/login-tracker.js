4b49b7c50feb10d8ec987ed8044edb93ad383be8
nov4 <kitsuika.who2@gmail.com>
2026-05-09 17:35:49 +0700

    Update login-tracker.js

--- netlify/functions/login-tracker.js
+++ netlify/functions/login-tracker.js
@@ -1,16 +1,19 @@
 exports.handler = async () => {
     const clientId = process.env.SPOTIFY_CLIENT_ID;
     
-    // Redirect langsung ke halaman utama
-    const redirectUri = 'https://novascatia.my.id/';
+    // Jika Client ID tidak terbaca di Netlify
+    if (!clientId) return { statusCode: 500, body: "Client ID Missing!" };
+
+    // Kita kembalikan ke callback backend
+    const redirectUri = 'https://novascatia.my.id/.netlify/functions/callback-tracker';
     const scope = 'user-read-private user-read-email user-read-recently-played user-top-read user-read-currently-playing';
 
-    // Pecah URL agar aman dari filter AI
     const p1 = "accounts"; const p2 = "spotify"; const p3 = "com";
     const authBase = "https://" + p1 + "." + p2 + "." + p3 + "/authorize?";
 
     const authUrl = authBase +
-        `response_type=code&client_id=${clientId}` +
+        `response_type=code` +
+        `&client_id=${clientId}` +
         `&scope=${encodeURIComponent(scope)}` +
         `&redirect_uri=${encodeURIComponent(redirectUri)}` +
         `&show_dialog=true`;
